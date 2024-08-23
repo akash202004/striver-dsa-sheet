e547003075bd270c032940fc9bd4323a570d11fd
akash202004 <akashlaha48@gmail.com>
2024-08-23 22:29:10 +0530

    union array easy

--- Array/Easy/findUnion.java
+++ Array/Easy/findUnion.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
 public class findUnion {
     public static void main(String[] args) {
-        System.out.println("AAKsh AJkdjk");
+        int arr1[] = {1, 2, 3, 4, 5};
+        int arr2 [] = {1, 2, 3, 6, 7};
+
+        ArrayList<Integer> unionResult = Union(arr1, arr2);
+
+        System.out.println(unionResult);
+    }
+    public static ArrayList<Integer> Union(int[] arr1, int[] arr2){
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < arr1.length; i++){
+            set.add(arr1[i]);
+        }
+
+        for(int i = 0; i < arr2.length; i++){
+            set.add(arr2[i]);
+        }
+
+        ArrayList<Integer> result = new ArrayList<>(set);
+//        Collections.sort(result);
+
+        return result;
+
     }
 }
