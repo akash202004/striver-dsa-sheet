0ff2954ee1a32ac80436f59b40df7868240878fe
akash202004 <akashlaha48@gmail.com>
2024-08-27 21:34:37 +0530

    missing number in array

---
+++ Array/Easy/missingNumber.java
@@ -0,0 +1,16 @@
+public class missingNumber {
+    public static void main(String[] args) {
+        int arr[] = {9,6,4,2,3,5,7,0,1};
+        int n = arr.length;
+        int result = missingNumberFromTheArrya(arr,n);
+        System.out.println(result);
+    }
+    public static int missingNumberFromTheArrya(int[] arr, int n) {
+        int sum = n * (n + 1) / 2;
+        int result = 0;
+        for(int i = 0; i < n; i++) {
+            result += arr[i];
+        }
+        return sum - result;
+    }
+}
