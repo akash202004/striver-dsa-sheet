e4c5096ba76a85a6bd5e76f16e8f30366f55fa9c
akash202004 <akashlaha48@gmail.com>
2024-09-04 01:06:30 +0530

    move zeroes -> array -> easy

--- Array/Easy/moveZeroes.java
+++ Array/Easy/moveZeroes.java
@@ -5,27 +5,19 @@ public class moveZeroes {
     public static void main(String[] args) {
         int arr[] = {0,1,0,3,12};
         moveZeroFunction(arr);
-        // Print the array to see the resul
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
     }
-    public static void moveZeroFunction(int[] nums){
-        Stack<Integer> sk = new Stack<>();
-        int count = 0;
+    public static void moveZeroFunction(int[] nums) {
+        int lastNumZero = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                count = count + 1;
-            }else{
-                sk.push(nums[i]);
+            if(nums[i] != 0){
+                nums[lastNumZero++] = nums[i];
             }
         }
-        for(int i  = 0; i < count; i++){
-            sk.push(0);
-        }
-        for(int i = nums.length; i <= 0; i--){
-            nums[i] = sk.peek();
-            sk.pop();
-        }
-        for(int i = 0; i < nums.length; i++){
-            System.out.print(nums[i] + " ");
+        for(int i = lastNumZero; i < nums.length; i++){
+            nums[i] = 0;
         }
     }
 }
