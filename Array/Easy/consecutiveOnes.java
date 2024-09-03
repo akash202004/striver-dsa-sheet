27b92ce7baca4879a7c1fffa72bd9861af7c5544
akash202004 <akashlaha48@gmail.com>
2024-09-03 09:48:14 +0530

    2Q array easy

---
+++ Array/Easy/consecutiveOnes.java
@@ -0,0 +1,14 @@
+public class consecutiveOnes {
+    public static void main(String[] args) {
+        int nums[] = {1,1,0,1,1,1};
+        int result = findConsecutive(nums);
+        System.out.println(result);
+    }
+    public static int findConsecutive(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        for(int num: nums)
+            maxCount = Math.max(maxCount, count = num == 0 ? 0 : count + 1);
+        return maxCount;
+    }
+}
---
+++ Array/Easy/singleNumber.java
@@ -0,0 +1,10 @@
+public class singleNumber {
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 0, 2, 1};
+        int result = 0;
+        for (int num : nums){
+            result ^= num;
+        }
+        System.out.println(result);
+    }
+}
